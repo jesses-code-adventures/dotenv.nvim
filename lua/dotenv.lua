7d2b3202093ec9ff0ac2551c9a74f4a63127e059
Jesse Williams <jesse@negativespacesounds.com>
2024-08-05 23:09:49 +1000

    batman

---
+++ lua/dotenv.lua
@@ -0,0 +1,51 @@
+local M = {}
+
+local function split_first(s, delimiter)
+    local delimiter_pos = string.find(s, delimiter, 1, true)
+    if not delimiter_pos then
+        return s
+    end
+    local key = string.sub(s, 1, delimiter_pos - 1)
+    local value = string.sub(s, delimiter_pos + 1)
+    return key, value
+end
+
+local function parse_value(value)
+    value = value:match("^%s*(.-)%s*$")
+    if (value:sub(1, 1) == '"' and value:sub(-1) == '"') or
+       (value:sub(1, 1) == "'" and value:sub(-1) == "'") then
+        value = value:sub(2, -2)
+    else
+        if value:find("=") then
+            error("Unquoted value contains an '=' sign: " .. value)
+        end
+    end
+    return value
+end
+
+function M.setup(opts)
+    local env_file = opts and opts.env_path and vim.fn.expand(opts.env_path) or vim.fn.stdpath("config") .. "/.env"
+    local file = io.open(env_file, "r")
+    if not file then
+      error("Couldn't locate a .env file in your nvim config when searching: " .. env_file)
+    end
+    for line in file:lines() do
+        if line == "" or line == nil then
+            goto continue
+        end
+        local parts = {split_first(line, "=")}
+        if #parts < 2 then
+            error("Incorrectly formatted line in .env file: " .. line)
+        end
+        local key, value = split_first(line, "=")
+        if not value then
+            error("Value is nil for line: " .. line)
+        end
+        value = parse_value(value)
+        vim.fn.setenv(key, value)
+        ::continue::
+    end
+    file:close()
+end
+
+return M
---
+++ readme.md
@@ -0,0 +1,59 @@
+# jesse's super simple .env file handler
+
+if you don't like setting your env variables in your rc files, you can use this.
+
+in the root of your nvim config, create a .env file and add your environment variables.
+
+if no .env file is found, this plugin will throw early.
+
+## lazy config
+
+```lua
+return {
+    {
+        "jesses-code-adventures/dotenv.nvim",
+        config = function()
+            vim.opt.runtimepath:append(vim.fn.expand(path))
+            require("dotenv").setup()
+        end,
+        lazy=false
+    }
+}
+```
+
+you may wish to provide a custom .env file path - you can use the env_path option to do this.
+
+```lua
+return {
+    {
+        "jesses-code-adventures/dotenv.nvim",
+        config = function()
+            vim.opt.runtimepath:append(vim.fn.expand(path))
+            require("dotenv").setup({
+                env_path="~/.config/nvim/.custom_env_file" -- this could be any file path
+            })
+        end,
+        lazy=false
+    }
+}
+```
+
+## formatting
+
+if your .env file is incorrectly formatted, this plugin will throw early.
+
+for example, this file will not work.
+
+```
+OPENAI_API_KEY=asdf=asdflj;
+```
+
+nor will this file.
+
+```
+OPENAI_API_KEY
+```
+
+## contributing
+
+please feel free to make any pull requests you'd like to. this currently works how i need it to work but i'm open to any ideas.
