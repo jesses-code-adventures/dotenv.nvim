412cdbe0a06fe20f9060d3c3d8db123aa7ed7fad
jesse <jesse@rapidpaylegal.com>
2024-09-11 11:53:46 +1000

    fix: exit early if no env file and not raise errors

--- lua/dotenv.lua
+++ lua/dotenv.lua
@@ -13,7 +13,7 @@ end
 local function parse_value(value)
     value = value:match("^%s*(.-)%s*$")
     if (value:sub(1, 1) == '"' and value:sub(-1) == '"') or
-       (value:sub(1, 1) == "'" and value:sub(-1) == "'") then
+        (value:sub(1, 1) == "'" and value:sub(-1) == "'") then
         value = value:sub(2, -2)
     end
     return value
@@ -22,16 +22,18 @@ end
 
 function M.setup(opts)
     local env_file = opts and opts.env_path and vim.fn.expand(opts.env_path) or vim.fn.stdpath("config") .. "/.env"
-    local raise_errors = opts and opts.raise_errors or true
+    local raise_errors = opts and opts.raise_errors and opts.raise_errors == true or true
     local file = io.open(env_file, "r")
-    if not file then
-      error("Couldn't locate a .env file in your nvim config when searching: " .. env_file)
+    if raise_errors and not file then
+        error("Couldn't locate a .env file in your nvim config when searching: " .. env_file)
+    elseif not raise_errors and not file then
+        return
     end
     for line in file:lines() do
         if line == "" or line == nil or line:match("^%s*#") then
             goto continue
         end
-        local parts = {split_first(line, "=")}
+        local parts = { split_first(line, "=") }
         if #parts < 2 and raise_errors then
             error("Incorrectly formatted line in .env file: " .. line)
         elseif not raise_errors then
