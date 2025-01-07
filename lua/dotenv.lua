e43aedfa06fb1921b7b23697a1e4c93363967f35
Jesse Williams <jesse@negativespacesounds.com>
2025-01-07 21:35:47 +1100

    update: also read env files in user's nvim config

--- lua/dotenv.lua
+++ lua/dotenv.lua
@@ -1,21 +1,20 @@
 local M = {}
 
 local function parse_line(line)
-    local key, value = line:match("^%s*([%w_]+)%s*=%s*(.*)%s*$")
-    if not key or not value then return nil, nil end
-    value = value:gsub('^["\'](.-)["\']$', "%1")
-    return key, value
+    local key, val = line:match("^%s*([%w_]+)%s*=%s*(.*)%s*$")
+    if not key or not val then return nil, nil end
+    val = val:gsub('^["\'](.-)["\']$', '%1')
+    return key, val
 end
 
 local function load_env_file(filepath)
     local env_vars = {}
-    local file = io.open(filepath, "r")
+    local file = io.open(filepath, 'r')
     if not file then return env_vars end
-
     for line in file:lines() do
-        local key, value = parse_line(line)
-        if key and value then
-            env_vars[key] = value
+        local k, v = parse_line(line)
+        if k and v then
+            env_vars[k] = v
         end
     end
     file:close()
@@ -23,23 +22,35 @@ local function load_env_file(filepath)
 end
 
 local function merge_envs(base_env, override_env)
-    for key, value in pairs(override_env) do
-        base_env[key] = value
+    for k, v in pairs(override_env) do
+        base_env[k] = v
     end
 end
 
-
 function M.setup(opts)
-    local cwd = vim.loop.cwd()
-    local env_files = opts.overrides or { ".env" }
+    opts = opts or {}
+    local config_dir = vim.fn.stdpath('config')
+    local config_files = opts.config_overrides or { '.env' }
+    local cwd_files = opts.overrides or { '.env' }
+
     local final_env = {}
-    for _, file in ipairs(env_files) do
-        local filepath = cwd .. "/" .. file
-        local env_vars = load_env_file(filepath)
+
+    -- load env from nvim config
+    for _, file in ipairs(config_files) do
+        local env_vars = load_env_file(config_dir .. '/' .. file)
         merge_envs(final_env, env_vars)
     end
-    for key, value in pairs(final_env) do
-        vim.fn.setenv(key, value)
+
+    -- now override from cwd
+    local cwd = vim.loop.cwd()
+    for _, file in ipairs(cwd_files) do
+        local env_vars = load_env_file(cwd .. '/' .. file)
+        merge_envs(final_env, env_vars)
+    end
+
+    -- apply final env
+    for k, v in pairs(final_env) do
+        vim.fn.setenv(k, v)
     end
 end
 
